7fef9135b92362a20240e4a7a5789ffa9753d1ee
Kum Jude Tem <judekum14@gmail.com>
2021-11-12 01:44:46 +0100

    Base template setup

--- TODO.md
+++ TODO.md
@@ -1,6 +1,13 @@
 # Postie
 
 First off is we start to build the scaffold of our app
-- create route and view files
-- then pull in tailwind `npm i tailwindcss` then run `npm install`
-- then run `npm run dev`
+
+-   create route and view files
+-   then pull in tailwind `npm i tailwindcss` then run `npm install`
+-   then run `npm run dev`
+
+-   pull in tailwind and modify the webpack.mix file
+-   go to resources/css/app.css and modify
+-   now run npm dev
+  
+- nxt modify the base template file and pull in tailwind
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Postie App</title>
+
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
-<body>
+<body class="bg-gray-300">
+  <nav class="p-6 bg-white flex justify-between mb-3">
+    <ul class="flex items-center">
+      <li><a href="" class="p-3">Home</a></li>
+      <li><a href="" class="p-3">Dashboard</a></li>
+      <li><a href="" class="p-3">Posts</a></li>
+    </ul>
+
+    <ul class="flex items-center">
+      <li><a href="" class="p-3">Jason Tem</a></li>
+      <li><a href="" class="p-3">Login</a></li>
+      <li><a href="" class="p-3">Login</a></li>
+      <li><a href="" class="p-3">Register</a></li>
+    </ul>
+  </nav>
+
   @yield('content')
 </body>
 
